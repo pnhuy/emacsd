1d898af254c289eb751d9aa8dccbb9fb433f3943
Huy Pham <keixi512@gmail.com>
2023-12-10 15:53:29 +0700

    Add phscroll submodules

--- org.el
+++ org.el
@@ -1,2 +1,10 @@
+;; load phscroll
+(add-to-list 'load-path (expand-file-name "packages/phscroll" user-emacs-directory))
+(require 'phscroll)
+;; uncomment the following line to enable phscroll in org-mode
+;; (setq org-startup-truncated nil)
+;; (with-eval-after-load "org"
+;;   (require 'org-phscroll))
+
 (setq org-adapt-indentation t)
 (setq org-list-allow-alphabetical t)
