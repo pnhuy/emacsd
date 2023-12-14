74c774ad2cca55a20540a0677572435063810129
Huy Pham <hpham@talosix.com>
2023-12-14 17:02:18 +0700

    Config to speed up

--- init.el
+++ init.el
@@ -3,6 +3,18 @@
 (load custom-file)
 (load (expand-file-name "init-packages.el" user-emacs-directory))
 
+;; speed up startup
+
+;; Minimize garbage collection during startup
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Lower threshold back to 8 MiB (default is 800kB)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (expt 2 23))))
+
+;; end speed up startup
+
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
 
